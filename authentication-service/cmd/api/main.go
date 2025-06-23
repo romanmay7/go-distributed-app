9089e5ec3fed7570b6e55f7937b8a745c9b10259
Roman M <romanmayers@yahoo.com>
2025-06-23 19:50:20 +0300

    Add Connection to Postgres, Update docker and make files

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -4,12 +4,19 @@ import (
 	"authentication/data"
 	"database/sql"
 	"fmt"
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 // Docker let multiple containers listen on the same port and threats them as individual services
 const webPort = "80"
+
+var counts int64
 
 type AuthServiceApp struct {
 	DB     *sql.DB
@@ -19,10 +26,16 @@ type AuthServiceApp struct {
 func main() {
 	log.Println("Starting authentication service")
 
-	// TODO connect to DB
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("Can't connect to Postgres!")
+	}
 
 	// set up config
-	app := AuthServiceApp{}
+	app := AuthServiceApp{
+		DB:     conn,
+		Models: data.New(conn),
+	}
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
@@ -31,6 +44,45 @@ func main() {
 
 	err := srv.ListenAndServe()
 	if err != nil {
+
 		log.Panic(err)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+
+	for { //Loop till we connect to DB. Try "counts" number of attempts(2 seconds each)
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Postgres not yet ready ...")
+			counts++
+		} else {
+			log.Println("Connected to Postgres!")
+			return connection
+		}
+
+		if counts > 10 {
+			log.Println(err)
+			return nil
+		}
+
+		log.Println("Backing off for two seconds....")
+		time.Sleep(2 * time.Second)
+		continue
 	}
 }
