af121f12278f63c6a6fbab025eaa1d61c082d660
Roman M <romanmayers@yahoo.com>
2025-07-19 17:28:55 +0300

    Add route & handlers for Logger on Broker ,update FrontEnd. Add logging on AuthService

--- authentication-service/cmd/api/handlers.go
+++ authentication-service/cmd/api/handlers.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
@@ -31,11 +33,43 @@ func (app *AuthServiceApp) Authenticate(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
+	//log the authentication request
+	err = app.logRequest("authentication", fmt.Sprintf("%s logged in", user.Email))
+
 	payload := jsonResponse{
 		Error:   false,
 		Message: fmt.Sprintf("Logged in user %s", user.Email),
 		Data:    user,
 	}
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 
 	app.writeJSON(w, http.StatusAccepted, payload)
+}
+
+// ----------------------------------------------------------------------------
+func (app *AuthServiceApp) logRequest(name, data string) error {
+	var entry struct {
+		Name string `json:"name"`
+		Data string `json:"data"`
+	}
+	entry.Name = name
+	entry.Data = data
+
+	jsonData, err := json.MarshalIndent(entry, "", "\t")
+	logServiceURL := "http://logger-service/log"
+
+	request, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{}
+	_, err = client.Do(request)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -10,11 +10,17 @@ import (
 type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
+	Log    LogPayload  `json:"log,omitempty"`
 }
 
 type AuthPayload struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LogPayload struct {
+	Email string `json:"name"`
+	Data  string `json:"data"`
 }
 
 /**************************************************************************************************/
@@ -40,11 +46,49 @@ func (app *BrokerApp) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 	switch requestPayload.Action {
 	case "auth":
 		app.authenticate(w, requestPayload.Auth)
+	case "log":
+		app.logItem(w, requestPayload.Log)
 	default:
 		app.errorJSON(w, errors.New("unknown action"))
 	}
 }
 
+func (app *BrokerApp) logItem(w http.ResponseWriter, logEntry LogPayload) {
+	jsonData, _ := json.MarshalIndent(logEntry, "", "\t")
+
+	logServiceURL := "http://logger-service/log"
+
+	request, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	defer response.Body.Close()
+
+	// make sure we get back the correct status code
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, err)
+		return
+	}
+
+	//Prepare Response
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "logged successfully"
+
+	app.writeJSON(w, http.StatusAccepted, payload)
+}
+
+// --------------------------------------------------------------------------------------------
 func (app *BrokerApp) authenticate(w http.ResponseWriter, a AuthPayload) {
 	// create some json we'll send to the auth microservice
 	jsonData, _ := json.MarshalIndent(a, "", "\t")
